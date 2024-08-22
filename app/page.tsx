b9f453f9b2aea444609b2a3779d28599ad0d71cf
nnnnnnn0090 <nnnnnnn0090@gmail.com>
2024-08-23 04:03:24 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import Link from 'next/link';
+import { ModeToggle } from "@/app/dark-toggle"
+
+import nextConfig from "../next.config.mjs";
+const BASE_PATH = nextConfig.basePath || "";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="relative z-[-1] flex items-center justify-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src={`${BASE_PATH}/logo.png`}
+          alt="Next.js Logo"
+          width={380}
+          height={40}
+          priority
+        />
+      </div>
+
+      <div className="mt-8 flex space-x-4">
+        <Link href="https://github.com/nnnnnnn0090" passHref>
+          <p className="text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+            GitHub
+          </p>
+        </Link>
+        <Link href="https://x.com/nnnnnnn0090" passHref>
+          <p className="text-gray-800 dark:text-gray-200 hover:text-blue-400 dark:hover:text-blue-300">
+            Twitter
+          </p>
+        </Link>
+        <Link href="https://zenn.dev/nnnnnnn0090" passHref>
+          <p className="text-gray-800 dark:text-gray-200 hover:text-blue-400 dark:hover:text-blue-300">
+            Zenn
+          </p>
+        </Link>
+      </div>
+
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
+    </main>
+  );
+}
