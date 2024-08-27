8c5d315114f0e09386951dd320c6dd796262dc6c
nnnnnnn0090 <nnnnnnn0090@gmail.com>
2024-08-28 05:07:47 +0900

    link

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import React from 'react';
 import markdownToHtml from 'zenn-markdown-html';
 import 'zenn-content-css'
 import {useEffect} from "react";
 import initTwitterScriptInner from "zenn-embed-elements/lib/init-twitter-script-inner";
+import Head from "next/head";
 
 type MarkdownRendererProps = {
   children: string;
@@ -14,11 +17,14 @@ export default async function MarkdownRenderer({ children: markdown }: MarkdownR
   }, []);
   
   const content = await markdownToHtml(markdown || '', {
-    embedOrigin: "https://embed.zenn.studio",
+    // embedOrigin: "https://embed.zenn.studio",
   })
 
   return (
     <article className="mb-32 znc">
+      <Head>
+        <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
+      </Head>
       <script
           dangerouslySetInnerHTML={{
             __html: initTwitterScriptInner
